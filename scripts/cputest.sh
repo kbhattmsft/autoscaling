1c9d1df5be493c751839f989715cb561ac2617a5
Kaushik Bhattacharya <kbhatt@microsoft.com>
2017-02-16 15:38:24 +0100

    added cputest and visualizer

---
+++ scripts/cputest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set x
+echo =====
+echo Check and create network...
+echo =====
+
+TEST_NET=cputest
+docker network ls | grep $TEST_NET
+RC=$?
+if [ $RC != 0 ]; then
+  docker network create -d overlay --label cputest $TEST_NET
+fi
+
+docker \
+  service create --name cputest \
+  --mode global \
+  --network TEST_NET \
+  --label com.docker.stack.namespace=cputest \
+  --container-label com.docker.stack.namespace=cputest \
+  -e STRESS_SYSTEM_FOR=10m \
+  -e  MAX_CPU_CORES=2 \
+  petarmaric/docker.cpu-stress-test
---
+++ scripts/setup_visualizer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set x
+echo =====
+echo Install Docker Swarm Mode Visualizer
+echo (Requires \"local\" docker daemon if run on Swarm Master and needs to attach on docker0)
+echo =====
+DOCKER0=172.17.0.1 
+docker \
+  run -it -d -p 8180:8080 \
+  -e HOST=$DOCKER0 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  manomarks/visualizer

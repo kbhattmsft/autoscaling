96816776c1f684f53d85c40eb3f6c8aba9d6b50d
Dwai Banerjee <dwaiba@microsoft.com>
2017-03-29 08:04:49 +0200

    Update setup_visualizer.sh

--- scripts/setup_visualizer.sh
+++ scripts/setup_visualizer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
 echo =====
-echo Install Docker Swarm Mode Visualizer
+echo Install Docker Swarm Mode Visualizer with external port 8087
 echo =====
 docker service create \
   --name=viz \
-  --publish=8080:8080/tcp \
+  --publish=8087:8080/tcp \
   --constraint=node.role==manager \
   --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
   manomarks/visualizer
